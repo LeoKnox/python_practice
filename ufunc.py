dc8ec86bc5ceded7eee156d187259ac833cd2a3b
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-06 19:05:04 -0700

    Add files via upload

--- ufunc.py
+++ ufunc.py
@@ -6,9 +6,12 @@ class Adventurer:
         self.name = name
         self.skill = skill
         self.lvl = lvl
+    
+    def identfunc(self):
+        print("I am " + self.name + "and I use " + self.skill + "!")
 
 x = SetX()
 a1 = Adventurer("Eveehi","Sword",3)
 a2 = Adventurer("Aelien","Spear",3)
-print(a1.name)
-print(a2.name + " " + a2.skill)
+a1.identfunc()
+a2.identfunc()
