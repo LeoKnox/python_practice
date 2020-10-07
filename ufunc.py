2c2108049b228359f42edec5a9b82bcb0d1fdaf6
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-06 19:01:07 -0700

    Add files via upload

--- ufunc.py
+++ ufunc.py
@@ -1,5 +1,14 @@
 class SetX:
     x = 5
 
+class Adventurer:
+    def __init__(self, name, skill, lvl):
+        self.name = name
+        self.skill = skill
+        self.lvl = lvl
+
 x = SetX()
-print(x.x)
+a1 = Adventurer("Eveehi","Sword",3)
+a2 = Adventurer("Aelien","Spear",3)
+print(a1.name)
+print(a2.name + " " + a2.skill)
