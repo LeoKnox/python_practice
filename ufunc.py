3725ab64fae1804c94b48787f7ede56fc676070f
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-07 18:13:44 -0700

    Add files via upload

--- ufunc.py
+++ ufunc.py
@@ -11,11 +11,14 @@ class Adventurer:
         print("I am " + self.name + " and I use " + self.weapon + "!")
 
 class Fighter(Adventurer):
-    pass
+    def __init__(self, name, weapon, lvl, subclass):
+        super().__init__(name, weapon, lvl)
+        self.subclass = subclass
 
 x = SetX()
-a1 = Fighter("Eveehi","Sword",3)
-a2 = Fighter("Aelien","Spear",3)
+a1 = Fighter("Eveehi","Sword",3, "Defender")
+a2 = Fighter("Aelien","Spear",3, "Weapon Master")
 a1.skill = "Hammer"
+print(a1.subclass)
 a1.identfunc()
 a2.identfunc()
