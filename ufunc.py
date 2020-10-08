095ec42bcbf4d1ca22ce64c641ff2178d82aeab8
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-07 18:54:33 -0700

    Add files via upload

--- ufunc.py
+++ ufunc.py
@@ -10,15 +10,26 @@ class Adventurer:
     def identfunc(self):
         print("I am " + self.name + " and I use " + self.weapon + "!")
 
+class Scout(Adventurer):
+    def __init__(self, name, weapon, lvl, specialty):
+        super().__init__(name, weapon, lvl)
+        self.specialty = specialty
+    
+    def subclassattack(self):
+        print(f"{self.name} attack with {self.specialty} and {self.weapon}")
+
 class Fighter(Adventurer):
     def __init__(self, name, weapon, lvl, subclass):
         super().__init__(name, weapon, lvl)
         self.subclass = subclass
+    
+    def subclassattack(self):
+        print(f"{self.name} attack uses {self.subclass} with {self.weapon}")
 
 x = SetX()
-a1 = Fighter("Eveehi","Sword",3, "Defender")
+a1 = Scout("Eveehi","Sword",3, "Sniper")
 a2 = Fighter("Aelien","Spear",3, "Weapon Master")
 a1.skill = "Hammer"
-print(a1.subclass)
-a1.identfunc()
-a2.identfunc()
+print(a1.weapon)
+a1.subclassattack()
+a2.subclassattack()
