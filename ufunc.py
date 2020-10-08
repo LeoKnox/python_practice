379556f4fa78a0b9895c2006933c25c4e747bb29
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-07 18:07:14 -0700

    Add files via upload

--- ufunc.py
+++ ufunc.py
@@ -1,21 +1,21 @@
 class SetX:
     x = 5
 
-class Undead:
-    pass
-
 class Adventurer:
-    def __init__(self, name, skill, lvl):
+    def __init__(self, name, weapon, lvl):
         self.name = name
-        self.skill = skill
+        self.weapon= weapon
         self.lvl = lvl
     
     def identfunc(self):
-        print("I am " + self.name + "and I use " + self.skill + "!")
+        print("I am " + self.name + " and I use " + self.weapon + "!")
+
+class Fighter(Adventurer):
+    pass
 
 x = SetX()
-a1 = Adventurer("Eveehi","Sword",3)
-a2 = Adventurer("Aelien","Spear",3)
+a1 = Fighter("Eveehi","Sword",3)
+a2 = Fighter("Aelien","Spear",3)
 a1.skill = "Hammer"
 a1.identfunc()
 a2.identfunc()
