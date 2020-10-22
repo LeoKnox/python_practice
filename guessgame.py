7857a87cc90b86d776222232f8d976e976f52b61
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-21 18:06:22 -0700

    Update guessgame.py

--- guessgame.py
+++ guessgame.py
@@ -3,4 +3,8 @@ import random
 answer = random.randint(0, 100)
 guess = input('Enter a guess:')
 guess = int(guess)
-print(guess)
+
+while guess != answer:
+    guess = input(str(answer) + 'Enter another guess: ')
+    guess = int(guess)
+print ("win")
