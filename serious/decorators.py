c13249bd03e39722e85242b480ace0c991bde65f
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-30 19:28:39 -0700

    Add files via upload

--- serious/decorators.py
+++ serious/decorators.py
@@ -1,3 +1,17 @@
+_functions = {}
+def register(f):
+    global _functions
+    _functions[f.__name__] = f
+    return f
+
+@register
+def foo():
+    return 'bar'
+
+foo = register(foo)
+print (foo)
+
+'''
 def identity(f):
     return f
 
@@ -5,5 +19,6 @@ def identity(f):
 def foo():
     return 'bar'
 
-foo = identity(foo)
+foo = identity('foo')
 print(foo)
+'''
