1a25ee729d824b8573a878555386afdb54f74ad7
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-03 20:55:37 -0800

    Update combat.py

--- combat.py
+++ combat.py
@@ -10,6 +10,7 @@ def hit(off_atk, def_ac):
     roll = random.randint(1, 7) + off_atk
     if roll > def_ac:
         print("hit")
+        print("Damage: " + str(random.randint(1,9)))
     else:
         print ("swish")
     print(roll)
