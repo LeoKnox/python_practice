3eeae5404e798d3ef481f77dcc6fdbbb25d54d58
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-03 20:44:11 -0800

    Create combat.py

---
+++ combat.py
@@ -0,0 +1,10 @@
+class MyChar:
+    def __init__(self, name, atk, ac):
+        self.name = name
+        self.atk = atk
+        self.ac = ac
+
+c1 = MyChar("Elric", 12, 12)
+
+print (c1.name)
+print (c1.atk + c1.ac
