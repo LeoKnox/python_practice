e896b77e27406eb4f9ee995b4e161da95e8450f8
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-03 20:52:55 -0800

    Update combat.py

--- combat.py
+++ combat.py
@@ -8,10 +8,15 @@ class MyChar:
 
 def hit(off_atk, def_ac):
     roll = random.randint(1, 7) + off_atk
+    if roll > def_ac:
+        print("hit")
+    else:
+        print ("swish")
     print(roll)
 
 c1 = MyChar("Elric", 12, 12)
+c2 = MyChar("Yrkon", 11, 11)
 
 print (c1.name)
 print (c1.atk + c1.ac)
-hit(3,3)
+hit(c1.atk,c2.ac)
