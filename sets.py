d7708938c4b91244efbfabfe94e20730a4d9ad7e
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-05 22:44:01 -0800

    Update sets.py

--- sets.py
+++ sets.py
@@ -1,3 +1,9 @@
+a = { 4, 5, 6 }
+b = { 5, 6, 7 }
+
+print (a.union(b))
+
+'''
 inter = {7, 8, 9 } & {5, 6, 7}
 union = {7, 8, 9 } | { 5,6,7}
 print(2 in {1, 2, 3, 5, 8})
@@ -6,3 +12,4 @@ sim_diff = {1, 3, 5, 7, 9 } ^ { 1, 2, 3, 5, 8 }
 print(inter)
 print(union)
 print(sim_diff)
+'''
