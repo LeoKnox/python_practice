f6586346de6ed8bfcc8e837273de6b597047cf88
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-09 21:44:40 -0800

    Update read_only.py

--- read_only.py
+++ read_only.py
@@ -7,3 +7,26 @@ class Character(object):
     @property
     def hp(self):
         return self._hp
+    
+    @property
+    def name(self):
+        return self.name
+    
+    def take_damage(self, damage):
+        self.hp -= damage
+        self.hp = 0 if self.hp <0 else self.hp
+    
+    @property
+    def is_alive(self):
+        return self.hp !=0
+    
+    @property
+    def is_wounded(self):
+        return self.hp < self.max_hp if self.hp > 0 else False
+    
+    @property
+    def Is_dead(self):
+        return not self.is_alive
+
+elric = Character('Elric', 100)
+print(elric.hp)
