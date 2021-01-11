63404769d1338ba9ce13479b5805dd6a40b155c6
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-10 20:58:20 -0800

    Update list_comp.py

--- list_comp.py
+++ list_comp.py
@@ -7,3 +7,7 @@ print(chars)
 two_by_two = [[1, 2, 3], [39, 4, 5], [6, 76, 8]]
 
 print([max(row) for row in two_by_two])
+
+char_name = ["Elric", "Red Sonja", "Conan", "Aelien"]
+
+print([name for name in char_name if name.startswith('E') or len(name) > 5])
