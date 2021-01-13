95adcdd9b3de1196f4f2f2ce58f148ac4dc012cd
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-12 22:08:01 -0800

    Update kwargs.py

--- kwargs.py
+++ kwargs.py
@@ -3,3 +3,9 @@ def work_args(**kwargs):
         print ("{0} = {1}".format(key, kwargs[key]))
 
 work_args(one = "1", two = "2", three = "3")
+
+z = "zee"
+
+a = "aaa"
+
+print("{0} and {1}".format(z, a))
