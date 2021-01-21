48a7c0fc87604befc61f02e2e539c2baf13be681
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-20 22:30:20 -0800

    Add files via upload

--- mysql_a.py
+++ mysql_a.py
@@ -11,4 +11,13 @@ mycursor = mydb.cursor()
 
 #mycursor.execute("CREATE TABLE characters (name VARCHAR(255), class VARCHAR(255))")
 
-mycursor.execute("ALTER TABLE characters ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+#mycursor.execute("ALTER TABLE characters ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+
+sql = "INSERT INTO characters (name, class) VALUES (%s, %s)"
+val = ("Aelien", "Fighter")
+
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, " number of rows")
