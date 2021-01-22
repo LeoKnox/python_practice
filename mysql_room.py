85575f104fa0aa2fb04e93f055cc39f519af59f5
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-21 16:46:36 -0800

    Add files via upload

--- mysql_room.py
+++ mysql_room.py
@@ -11,5 +11,19 @@ mycursor = mydb.cursor()
 #mycursor.execute("CREATE TABLE mysql_room (name VARCHAR(50), description VARCHAR(255), length INT, width INT)")
 #mycursor.execute("ALTER TABLE mysql_room ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
 
-mycursor.execute("CREATE TABLE mysql_doors (door_id INT AUTO_INCREMENT PRIMARY KEY, wall VARCHAR(50), location INT, room_one INT, room_two INT)")
-mycursor.execute("RENAME TABLE mysql_room TO mysql_rooms")
+#mycursor.execute("CREATE TABLE mysql_doors (door_id INT AUTO_INCREMENT PRIMARY KEY, wall VARCHAR(50), location INT, room_one INT, room_two INT)")
+#mycursor.execute("RENAME TABLE mysql_room TO mysql_rooms")
+
+sql = "INSERT INTO mysql_rooms (name, description, length, width) VALUES (%s, %s, %s, %s)"
+val = ("Entry", "Entrance", 5, 5)
+mycursor.execute(sql, val)
+
+sql = "INSERT INTO mysql_doors (wall, location, room_one, room_two) VALUES (%s, %s, %s, %s)"
+val = [
+    ('north', 4, 1, 2),
+    ('west', 3, 2, 1)
+]
+
+mycursor.executemany(sql, val)
+
+mydb.commit()
