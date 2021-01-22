5edde54a9d81ca3c21c7e52b9c5bad62bb21e191
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-21 16:13:59 -0800

    Add files via upload

--- mysql_room.py
+++ mysql_room.py
@@ -3,9 +3,10 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "root"
+    password = "root",
+    database = "mysql_room"
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE mysql_room")
+mycursor.execute("CREATE TABLE mysql_room (name VARCHAR(50), description VARCHAR(255), length INT, width INT)")
