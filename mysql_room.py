2d638530df3560b74326dbec66ab4c7ed4e1e8b7
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-21 16:34:06 -0800

    Add files via upload

--- mysql_room.py
+++ mysql_room.py
@@ -1,5 +1,4 @@
 import mysql.connector
-
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
@@ -9,4 +8,8 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE mysql_room (name VARCHAR(50), description VARCHAR(255), length INT, width INT)")
+#mycursor.execute("CREATE TABLE mysql_room (name VARCHAR(50), description VARCHAR(255), length INT, width INT)")
+#mycursor.execute("ALTER TABLE mysql_room ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+
+mycursor.execute("CREATE TABLE mysql_doors (door_id INT AUTO_INCREMENT PRIMARY KEY, wall VARCHAR(50), location INT, room_one INT, room_two INT)")
+mycursor.execute("RENAME TABLE mysql_room TO mysql_rooms")
