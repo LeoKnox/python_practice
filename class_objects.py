c184c8d5073f8e58aa719b9bb14688515bc6d562
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-24 23:12:35 -0800

    Update class_objects.py

--- class_objects.py
+++ class_objects.py
@@ -2,6 +2,9 @@ class CharClass:
     def __init__(self, name, lvl):
         self.name = name
         self.lvl = lvl
+    
+	def charDescription(self):
+		print("Char name: " + self.name + " is " + self.lvl)
 
 c1 = CharClass("Eveehi", 1)
-print (c1.lvl)
+c1.charDescription()
