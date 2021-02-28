343476dfff127afd3ac6ddf28f95cc7c32209f57
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-27 20:53:28 -0800

    Add files via upload

--- classes.py
+++ classes.py
@@ -6,7 +6,15 @@ class Char_Class:
     
     def char_method(self):
         print('Hail, I am adventurer', self.name)
+    
+    def addArmor(self, armor):
+        self.armor = armor
+    
+    def displayarmor(self):
+        return self.armor
 
 obj = Char_Class("Elric")
 obj.char_method()
 print(obj.hp)
+obj.addArmor("Plate")
+print("Wearing armor", obj.displayarmor())
