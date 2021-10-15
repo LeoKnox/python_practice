97936b778183fa90f96e2b3ec863acd886055284
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-14 21:23:33 -0700

    Update string.py

--- string.py
+++ string.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 word_one = "word!"
 word_two = "Hello"
 
@@ -10,3 +12,13 @@ def retro():
 first, second, third = retro()
   
 print(first, second, third)
+
+
+def find_anagram(first_str, second_str):
+     return Counter(first_str) == Counter(second_str)
+  
+def find_anagram(first_str, second_str): 
+    return sorted(first_str) == sorted(second_str) 
+  
+print(find_anagram('taco', 'coat'))
+print(find_anagram('bleak', 'brake'))  
