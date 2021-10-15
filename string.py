155a256a91142934adaae7215bc1e49034cce36c
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-14 21:18:56 -0700

    Update string.py

--- string.py
+++ string.py
@@ -4,3 +4,9 @@ word_two = "Hello"
 word_one, word_two = word_two, word_one
 
 print(str(word_one) + ', ' + str(word_two))
+
+def retro():
+    return 'a', 'b', 'c'
+first, second, third = retro()
+  
+print(first, second, third)
