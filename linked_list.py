4fee42f6212ed164d2761c0f4c0f87b2051adf58
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-15 21:43:08 -0700

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -7,9 +7,10 @@ class linked_list:
 	def __init__(self):
 		self.head = None
 
-	def add_node(data):
+	def add_node(self, data):
 		new_node = linked_node(data)
-		this.linked_node = linked_node()
+		linked_list = linked_node()
 		print ("test")
 
 newList = linked_list()
+newList.add_node(5)
