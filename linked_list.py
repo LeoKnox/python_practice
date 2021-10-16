e59f3016f7efdaa3c62e7e7241ca8b61e19151cd
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-15 21:28:35 -0700

    Create linked_list.py

---
+++ linked_list.py
@@ -0,0 +1,10 @@
+class linked_node:
+	def __init__(self, value=None):
+		self.value = value
+		self.next = None
+
+class linked_list:
+	def __init__(self):
+		self.head = None
+
+newList = linked_list()
