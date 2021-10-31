a92c03a5e93a65d6d04d6fa3f8b5223300932d59
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-30 19:54:00 -0700

    Create first.js

---
+++ React/first.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello New React Project</h1>
+      <h2>Writing React Code</h2>
+    </div>
+  );
+}
