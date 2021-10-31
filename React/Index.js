e2534da393565d45d493a967c4d16dfa9b04bd8d
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-30 20:15:13 -0700

    Update Index.js

--- React/Index.js
+++ React/Index.js
@@ -1,12 +1,13 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import "./styles.css";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+export default function App() {
+  const text = 'one' in this.state ? this.state.text : this.props.text;
+  return (
+    <div className="App">
+      <h1>Hello New React Project</h1>
+      <h2>Writing React Code</h2>
+      <textarea value={text} onChange={event => this.onTextChange(event)} />
+      <p>{text.length}</p>
+    </div>
+  );
+}
