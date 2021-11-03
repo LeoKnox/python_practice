49243a7fbdb5c296ebb34c56f1ace0923ba49a64
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-02 23:08:51 -0700

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,36 +1,40 @@
 def merge_sort(sort_arr):
-	mid = len(sort_arr)//2;
-	print(mid)
+	if (len(sort_arr) > 1):
+		mid = len(sort_arr)//2;
+		print(mid)
 
-	left = sort_arr[:mid]
-	right = sort_arr[mid:]
+		left = sort_arr[:mid]
+		right = sort_arr[mid:]
 
-	merge_sort(left)
-	merge_sort(right)
+		merge_sort(left)
+		merge_sort(right)
 
-	print(left)
-	print(right)
+		print(left)
+		print(right)
 
-	x = y = z = 0
+		x = y = z = 0
 
-	while (x < len(left) and y < len(right)):
-		if left[x] < right[y]:
+		while (x < len(left) and y < len(right)):
+			print("while 1")
+			if left[x] < right[y]:
+				sort_arr[z] = left[x]
+				x += 1
+			else:
+				sort_arr[z] = right[x]
+				y += 1
+			z += 1
+
+		while (x < len(left)):
+			print("while 2")
 			sort_arr[z] = left[x]
 			x += 1
-		else:
-			sort_arr[z] = right[x]
+			z += 1
+
+		while (y < len(right)):
+			print("while 3")
+			sort_arr[z] = right[y]
 			y += 1
-		z += 1
-
-	while (x < len(left)):
-		sort_arr[z] = left[x]
-		x += 1
-		z += 1
-
-	while (y < len(right)):
-		sort_arr[z] = right[y]
-		y += 1
-		z += 1
+			z += 1
 
 start_arr = [5,9,2,8,4,3,1]
 merge_sort(start_arr)
