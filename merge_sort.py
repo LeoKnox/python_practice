5dc0dde8c738e0378860cdb0dd106213a608f245
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-02 22:52:11 -0700

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -8,6 +8,17 @@ def merge_sort(sort_arr):
 	print(left)
 	print(right)
 
+	x = y = z = 0
+
+	while (x < len(left) and y < len(right)):
+		if left[x] < right[y]:
+			sort_arr[z] = left[x]
+			x += 1
+		else:
+			sort_arr[z] = right[x]
+			y += 1
+		z += 1
+
 start_arr = [5,9,2,8,4,3,1]
 merge_sort(start_arr)
 
