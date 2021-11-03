6cf4712fce1150bb84cf6462546213a8ce5250f8
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-02 22:43:46 -0700

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -2,7 +2,10 @@ def merge_sort(sort_arr):
 	mid = len(sort_arr)//2;
 	print(mid)
 
-start_arr = {5,9,2,8,4,3}
+	left = sort_arr[:mid]
+	right = sort_arr[mid:]
+
+start_arr = {5,9,2,8,4,3,1}
 merge_sort(start_arr)
 
 print(start_arr)
