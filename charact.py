ae19ae229ddc14331ff802ca8ea860eabb5d160a
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-15 13:18:27 -0800

    Create charact.py

---
+++ charact.py
@@ -0,0 +1,7 @@
+class Adventurer:
+  def __init__(self, name, specilization, race):
+    self.name = name
+    self.specilization = specilization
+    self.race = race
+
+myadv = Adventurer("Elric", "fighter/mage", "elf")
