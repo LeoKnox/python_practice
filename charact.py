d0cb8bd3baaaf510e7363ad173f13f7367f5a882
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-16 21:27:34 -0800

    Update charact.py

--- charact.py
+++ charact.py
@@ -6,7 +6,7 @@ class Adventurer:
     self.level = 1
 
   def show_level(self):
-    print(self.level)
+    print("You are level %s." %(self.level))
 
 myadv = Adventurer("Elric", "fighter/mage", "elf")
 myadv.show_level()
