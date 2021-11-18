2769dc3c11f172b270b80135d3b03c51734846e7
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-17 22:27:26 -0800

    Update charact.py

--- charact.py
+++ charact.py
@@ -15,7 +15,7 @@ class Adventurer:
     wt=[3, 5, 2]
     attack_location = random.choices(loc, wt)
     attack_roll = random.randrange(1,21,1)
-    print(f"{self.name} strikes to hit with a {attack_roll} at {attack_location}")
+    print(f"{self.name} strikes to hit with a {attack_roll} at {attack_location}{' critical!' if attack_roll == 20 else ''}")
 
 myadv = Adventurer("Elric", "fighter/mage", "elf")
 myadv.show_level()
