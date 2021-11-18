1192b544a86fa53dfdc4113b9965b3f5fd73d355
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-17 22:08:55 -0800

    Update charact.py

--- charact.py
+++ charact.py
@@ -1,3 +1,5 @@
+import random
+
 class Adventurer:
   def __init__(self, name, specilization, race):
     self.name = name
@@ -8,5 +10,10 @@ class Adventurer:
   def show_level(self):
     print(f"{self.name} you are level {self.level}.")
 
+  def attack(self):
+    attack_roll = random.randrange(1,21,1)
+    print(f"{self.name} strikes to hit with a {attack_roll}")
+
 myadv = Adventurer("Elric", "fighter/mage", "elf")
 myadv.show_level()
+myadv.attack()
