586c355ca503a8d3308a21588ffecbd415587549
LeoKnox <LeoKnox@users.noreply.github.com>
2021-12-10 20:40:46 -0800

    Update TicTacToe.js

--- React/TicTacToe.js
+++ React/TicTacToe.js
@@ -1,8 +1,14 @@
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value = null,
+    };
+  }
   render() {
     return (
-      <button className="square" onClick={ function() { console.log("click"); }}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({ value: "X"}) }>
+        {this.state.value}
       </button>
     );
   }
