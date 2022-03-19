7edf0806c6b5e35a077c0c6c55b339075d440b94
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-18 18:20:29 -0700

    Update second_lowest.py

--- second_lowest.py
+++ second_lowest.py
@@ -1,13 +1,14 @@
-    lowest = 0
-    second = 0
     for _ in range(int(input())):
         name = input()
         score = float(input())
+        lowest = {}
+        second = []
         print(name + " " + str(score))
-        if lowest == 0:
-            lowest = score
-        elif score < lowest:
+        if lowest:
             second = lowest
             lowest = score
+        else:
+            lowest = {name: score}
+            
     print (lowest)
-    print (second)
+    print (lowest.score)
