a9cc11741dad291df58141c7620c0f24a581736b
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-18 18:48:27 -0700

    Update second_lowest.py

--- second_lowest.py
+++ second_lowest.py
@@ -4,9 +4,14 @@
         lowest = {}
         second = []
         print(name + " " + str(score))
+        # check if lowest variable exists
         if lowest:
             second = lowest
             lowest = score
+            #compare if score is lower assign lowest to second lowest
+            #compare if score is lower then second score to second
+            #compare if score equals lowest add sorted to array
+        # if lowest is empty assign first score
         else:
             lowest = {name: score}
             
