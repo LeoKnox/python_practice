837f9d018d94cbc1972bee63064284e64d308f8a
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-19 21:20:38 -0700

    Update second_lowest.py

--- second_lowest.py
+++ second_lowest.py
@@ -1,22 +1,22 @@
+    lowest = []
+    second = []
     for _ in range(int(input())):
         name = input()
         score = float(input())
-        lowest = []
-        second = []
         print(name + " " + str(score))
         # check if lowest variable exists
         if len(lowest) > 0:
-            print("2")
             #compare if score is lower assign lowest to second lowest
             if (lowest[1] > score):
                 second = [lowest]
                 lowest[0] = name
                 lowest[1] = score
-            #compare if score is lower then second score to second
             #compare if score equals lowest add sorted to array
+            #compare if score is lower then second score to second
+            #elif (second[0][1] > score):
+            print(second)
         # if lowest is empty assign first score
         else:
-            print("1")
             lowest.append(name)
             lowest.append(score)
     print (lowest)
