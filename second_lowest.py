e24eb716e8cac68e7cacda8e20d8263cfd87c3ff
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-23 17:29:05 -0700

    Update second_lowest.py

--- second_lowest.py
+++ second_lowest.py
@@ -1,34 +1,38 @@
+if __name__ == '__main__':
     lowest = []
     second = []
     for _ in range(int(input())):
         name = input()
         score = float(input())
         student = (name, score)
-        print(name + " " + str(score))
+        #print(name + " " + str(score))
         # check if lowest variable exists
         if len(lowest) > 0:
             #compare if score is lower assign lowest to second lowest
             if (lowest[1] > student[1]):
-                print("*")
+                #print("*")
                 second[0] = (lowest[0], lowest[1])
                 lowest[0] = name
                 lowest[1] = score
-                print(lowest)
-                print(second)
+                #print(lowest)
+                #print(second)
             #compare if score equals lowest add sorted to array
             elif (second[0][1] == score):
-                print("&&&")
+                #print("&&&")
                 second.append((name, score))
             #compare if score is lower then second score to second
             elif (second[0][1] > score):
-                print("**")
-                print(second)
+                #print("**")
+                #print(second)
                 second = (name, score)
         # if lowest is empty assign first score
         else:
-            print("!")
+            #print("!")
             lowest.append(name)
             lowest.append(score)
             second = [lowest]
-    print (lowest)
+    #print (lowest)
     print (second)
+    second = second.sort()
+    for sec in second:
+        print(sec)
