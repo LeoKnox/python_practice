3c7fad750039decac24f75c6315db4c77684859d
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-25 21:36:01 -0700

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -1,7 +1,12 @@
-    n = int(raw_input())
+'''
+n = int(raw_input())
     integer_list = map(int, raw_input().split())
     tuple_list = ()
     tuple_list(tuple(integer_list))
     print('N:', n)
     print('il:', integer_list)
     print('h:', hash(tuple_list))
+'''
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+    print(hash(tuple(integer_list)))
