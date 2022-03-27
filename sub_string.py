9b659ff11ce30583573b68f2c3c99bbe2a24f9dc
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-27 13:13:39 -0700

    Update sub_string.py

--- sub_string.py
+++ sub_string.py
@@ -3,9 +3,11 @@ def count_substring(string, sub_string):
     sub_index = 0
     print(sub_string[sub_index])
     for i, ch in enumerate(string):
-        if ch == sub_string[sub_index]:
+        if ch == (sub_string[sub_index] and sub_index == len(sub_string-1)):
             sub_index = sub_index + 1
             print(sub_string)
         elif sub_index > 0:
             sub_index = 0
+        elif sub_index == len(sub_string)-1:
+            return position
     return position
