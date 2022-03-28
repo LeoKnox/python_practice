640f6532a7567007321604b829f5d568dbb9f9fe
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-28 13:02:39 -0700

    Update sub_string.py

--- sub_string.py
+++ sub_string.py
@@ -3,10 +3,10 @@ def count_substring(string, sub_string):
     sub_index = 0
     print(sub_string[sub_index])
     for i, ch in enumerate(string):
-        print(i, ch, sub_string[sub_index])
-        if ch == (sub_string[sub_index] and sub_index == len(sub_string)-1):
+        print(sub_index, ch, sub_string[sub_index])
+        if ch == sub_string[sub_index] and sub_index != len(sub_string)-1:
             sub_index = sub_index + 1
-            print(sub_string)
+            print("((",sub_string)
         elif sub_index > 0:
             sub_index = 0
         elif sub_index == len(sub_string)-1:
