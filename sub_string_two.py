37a157e21f9beaaa2ec9432a6af97c45bed04360
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-29 14:08:05 -0700

    Update sub_string_two.py

--- sub_string_two.py
+++ sub_string_two.py
@@ -1,4 +1,4 @@
 def count_substring(string, sub_string):
     print(len(string)-len(sub_string))
-    for ch in string[0:len(string)-len(sub_string):1]:
-        print(ch)
+    for i, ch in enumerate(string[0:len(string)-len(sub_string):1]):
+        print(string[i:i+3])
