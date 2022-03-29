f4408742da529de00164a425cb4156aec13651cc
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-29 16:11:58 -0700

    Update sub_string_two.py

--- sub_string_two.py
+++ sub_string_two.py
@@ -1,4 +1,9 @@
 def count_substring(string, sub_string):
-    print(len(string)-len(sub_string))
-    for i, ch in enumerate(string[0:len(string)-len(sub_string):1]):
-        print(string[i:i+3])
+    #print(len(string)-len(sub_string))
+    total = 0
+    for i, ch in enumerate(string[0:len(string)-len(sub_string)+1:1]):
+        if sub_string == string[i:i+len(sub_string)]:
+            total += 1
+            #print("yeah")
+        #print(string[i:i+len(sub_string)])
+    return total
