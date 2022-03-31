7f48d7d1e5065d24a80b4e073e2a09aea4123c7d
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-31 07:17:16 -0700

    Update string_val.py

--- string_val.py
+++ string_val.py
@@ -1,8 +1,16 @@
 if __name__ == '__main__':
     s = input()
+    ans = [False, False, False, False, False]
     for ch in s:
-    #print(s.isalnum())
-    #print(s.isalpha())
-        print(ch.isdigit())
-    #print(s.islower())
-    #print(s.isupper())
+        if ch.isalnum():
+            ans[0] = True
+        if ch.isalpha():
+            ans[1] = True
+        if ch.isdigit():
+            ans[2] = True
+        if ch.islower():
+            ans[3] = True
+        if ch.isupper():
+            ans[4] = True
+    for a in ans:
+        print(a)
