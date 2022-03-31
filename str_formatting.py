0b1f9948f224522523b777eb9c36e090f0b54e02
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-31 09:16:38 -0700

    Update str_formatting.py

--- str_formatting.py
+++ str_formatting.py
@@ -1,4 +1,6 @@
 def print_formatted(number):
+    width = len(str(bin(number)[2:]))
+    print(width)
     for i in range(number):
         i += 1
         print(i, str(oct(i)[2:]), str(hex(i)[2:]), str(bin(i)[2:]))
