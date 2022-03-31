98795e0c8ffab05493c0c68d90c61b051fd6dcb0
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-31 06:46:18 -0700

    Create string_val.py

---
+++ string_val.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    s = input()
+    for ch in s:
+    #print(s.isalnum())
+    #print(s.isalpha())
+        print(ch.isdigit())
+    #print(s.islower())
+    #print(s.isupper())
