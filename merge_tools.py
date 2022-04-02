bc607c0c9222d1b6334a7365a9a4a6e412d87c22
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-01 19:04:04 -0700

    Update merge_tools.py

--- merge_tools.py
+++ merge_tools.py
@@ -1,6 +1,6 @@
 def merge_the_tools(string, k):
     groups = [string[i:i+k] for i in range(0, len(string), k)]
+    ans = []
     for g in groups:
-        ans = ''.join(set(g))
-        print(ans)
+        ans = [ans.append(i) for i in g if i not in g]
     print(groups)
