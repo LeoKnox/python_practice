444e436bd45998e96983385f64d00168c61948f6
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-03 12:19:34 -0700

    Update second_lowest.py

--- second_lowest.py
+++ second_lowest.py
@@ -1,9 +1,14 @@
- if __name__ == '__main__':
     low = []
     ans = []
+    grades = {}
     for _ in range(int(input())):
         name = input()
         score = float(input())
+        if grades[score]:
+            print("dupe")
+        else:
+            grades[score]= name
         print(name, score)
         low.append((name, score))
     print(low)
+    print(grades)
