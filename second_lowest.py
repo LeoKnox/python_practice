ca30fdecd3af35e242de35cc3a5752831e08cc6b
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-03 18:51:14 -0700

    Update second_lowest.py

--- second_lowest.py
+++ second_lowest.py
@@ -1,10 +1,12 @@
 if __name__ == '__main__':
-    low = 100
-    second = 100
+    low = 10000
+    second = 10000
     grades = {}
     for _ in range(int(input())):
         name = input()
         score = float(input())
+        print(low, second)
+        print(grades)
         if score < low:
             second = low
             low = score
