d1e97a818645ed816e71646b1f12213ccd42e253
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-04 09:42:03 -0700

    Update list_func.py

--- list_func.py
+++ list_func.py
@@ -4,5 +4,6 @@ if __name__ == '__main__':
     print(N)
     for i in range(N):
         command = input()
-        ans[command](*args)
-        print(ans)
+        command.split(" ")
+        #ans[command](*args)
+        print(command)
