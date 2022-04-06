0da1f8b28b1e96b5b9e3638fd47c35bfddad3636
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-06 10:13:29 -0700

    Update iter_tools.py

--- iter_tools.py
+++ iter_tools.py
@@ -1,10 +1,12 @@
 a = input()
-b = input()
+#b = input()
 
-#print(a)
+print(a)
 #print(b)
 ans = ""
-for i in a[0:len(a)+1:2]:
-    for j in b[0:len(b)+1:2]:
+a = list(a)
+for i in a[0:len(a):1]:
+    for j in i[0:len(i):1]:
+        print(i, j)
         ans += "("+i+", "+j+")"+" "
 print(ans)
