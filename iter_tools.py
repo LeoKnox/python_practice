3c6ef9cdb6698dd3d00dbe999252153cd494432a
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-06 10:21:40 -0700

    Update iter_tools.py

--- iter_tools.py
+++ iter_tools.py
@@ -1,10 +1,9 @@
-a = input()
-#b = input()
+a = input().split()
+b = input().split()
 
-print(a)
-#print(b)
+print(list(a))
+print(b)
 ans = ""
-a = list(a)
 for i in a[0:len(a):1]:
     for j in i[0:len(i):1]:
         print(i, j)
