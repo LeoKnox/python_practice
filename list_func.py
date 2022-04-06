1c7332552970067fedce7b647f15215e2d83da9b
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-05 21:31:08 -0700

    Update list_func.py

--- list_func.py
+++ list_func.py
@@ -1,15 +1,12 @@
 if __name__ == '__main__':
     N = int(input())
     ans = []
-    #print(N)
     comm = ""
     for i in range(N):
         command = input()
         command = command.split(" ")
-        #ans[command](*args)
         comm = ",".join([i for i in command[1:]])
-        #print(eval(command[0], *command[1:]), comm)
-        print(command[0], comm)
         if command[0] != "print":
             eval("ans." + command[0] + "(" + comm + ")")
-        print(ans)
+        else:
+            print(ans)
