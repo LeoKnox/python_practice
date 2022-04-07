938117d951c2ee78e68608cabdad60979f4ffdf5
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-07 11:26:06 -0700

    Update iter_tools.py

--- iter_tools.py
+++ iter_tools.py
@@ -1,6 +1,7 @@
 def recur_iter(a, b):
     if type(a) == list:
-        for i in a:
+        for i in a[0]:
+            print('a', i)
             recur_iter(i, b)
     else:
         print(b, a)
@@ -12,4 +13,14 @@ b = input().split()
 #print(list(a))
 #print(b)
 ans = ""
+print(type(a))
+if (type(a) == list):
+    print('stet')
 recur_iter(a, 8)
+'''
+for i in a[0:len(a):1]:
+    for j in b[0:len(b):1]:
+        #print(i, j)
+        ans += "("+i+", "+j+")"+" "
+print(ans)
+'''
