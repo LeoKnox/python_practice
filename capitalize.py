314efcf374ef122ad869e9fe71f37b4fb9e4e93d
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-07 18:09:48 -0700

    Update capitalize.py

--- capitalize.py
+++ capitalize.py
@@ -1,7 +1,7 @@
 def solve(s):
-    s = str(s)
+    print("***", type(s))
     for i, c in enumerate(s):
         if i == 0:
             print(s[i].upper())
-            s = s[:i] + s[i].upper() + str[i+1:]
+            s = s[i].upper() + str[i+1:]
     return(s)
