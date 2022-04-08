014161275914689234f6a2c9b5344dc2aea0aa22
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-08 10:31:37 -0700

    Update capitalize.py

--- capitalize.py
+++ capitalize.py
@@ -3,5 +3,5 @@ def solve(s):
     for i, c in enumerate(s):
         if i == 0:
             print(s[i].upper())
-            s = s[i].upper() + str[i+1:]
+            s = s[:i] + s[i].upper() + s[i+1:]
     return(s)
