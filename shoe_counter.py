a99e6663593837de5d7934cc5eec81f29f78e13e
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-08 13:43:08 -0700

    Update shoe_counter.py

--- shoe_counter.py
+++ shoe_counter.py
@@ -1,5 +1,8 @@
 cnt = input()
 new_arr = input()
-print(cnt, new_arr)
+print(type(new_arr), new_arr)
+print(new_arr.find("6"))
+for j in new_arr:
+    print("!!!!", j)
 for i in range(2,int(cnt)):
     print("*", i)
