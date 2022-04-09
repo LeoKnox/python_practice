109ccd0c5281bf95eb1cd69ffa561b9468ba0818
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-09 14:40:08 -0700

    Update shoe_counter.py

--- shoe_counter.py
+++ shoe_counter.py
@@ -1,8 +1,5 @@
+from collections import Counter
+
 cnt = input()
-new_arr = input()
-print(type(new_arr), new_arr)
-print(new_arr.find("6"))
-for j in new_arr:
-    print("!!!!", j)
-for i in range(2,int(cnt)):
-    print("*", i)
+new_arr = Counter(input())
+print(new_arr)
