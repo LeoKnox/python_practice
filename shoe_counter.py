fbbb34008fc820a87695a09358c8908f58451943
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-09 14:45:54 -0700

    Update shoe_counter.py

--- shoe_counter.py
+++ shoe_counter.py
@@ -2,4 +2,12 @@ from collections import Counter
 
 cnt = input()
 new_arr = Counter(input())
+sum = 0
 print(new_arr)
+
+for i in range(0, int(cnt)-3):
+    ans = input()
+    print(ans)
+    sum += new_arr[i]
+    new_arr[i] -= 1
+print (sum)
