fd6874efb736a0765f0302a4ce7844c57ed28832
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-13 10:38:29 -0700

    Update counter.py

--- counter.py
+++ counter.py
@@ -3,5 +3,8 @@ from collections import Counter
 
 our_in = [];
 for i in sys.stdin:
-    our_in.append(i);
-print(Counter(our_in[1]))
+    our_in.append(i)
+new_cnt = our_in.pop()
+new_cnt = Counter(our_in.pop())
+for j in our_in:
+    print(j)
