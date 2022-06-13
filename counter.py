418dc1b22d7ff4d84dc9419a2b14309dcdd93db1
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-13 12:05:46 -0700

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,10 +1,14 @@
 import sys
 from collections import Counter
 
+def price_shoe(order):
+    o = order.split(" ")
+    return o
+    
 our_in = [];
 for i in sys.stdin:
     our_in.append(i)
 new_cnt = our_in.pop()
 new_cnt = Counter(our_in.pop())
-for j in our_in:
-    print(j)
+total = map(price_shoe, our_in)
+print(total)
