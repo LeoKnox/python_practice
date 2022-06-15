0114c0563d20527a6d52db1a0b68c337e749c618
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-14 21:17:30 -0700

    Update mult_array.py

--- mult_array.py
+++ mult_array.py
@@ -4,6 +4,4 @@ from itertools import product
 x = []
 x.append(input().split(' '))
 x.append(input().split(' '))
-#print(list(product(*x)))
-for p in list(product(*x)):
-    print(p)
+print (list(product(*x)))
