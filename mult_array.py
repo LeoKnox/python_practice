4c7f6304dcd410c758670cd8f6a809ac12c457b7
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-14 21:22:40 -0700

    Update mult_array.py

--- mult_array.py
+++ mult_array.py
@@ -2,7 +2,6 @@ import sys
 from itertools import product
 
 x = []
-y = []
 x.append(input().split(' '))
-y.append(input().split(' '))
-print(list(product(x*y)))
+x.append(input().split(' '))
+print(list(product(x)))
