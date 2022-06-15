d352f5443e11ffd9d6416b424effab9606d7c50e
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-14 21:35:34 -0700

    Update mult_array.py

--- mult_array.py
+++ mult_array.py
@@ -2,6 +2,6 @@ import sys
 from itertools import product
 
 x = []
-x.append(input().split())
-x.append(input().split())
+x.append(map(int, input().split()))
+x.append(map(int, input().split()))
 print(' '.join([str(y) for y in (list(product(*x)))]))
