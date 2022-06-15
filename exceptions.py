fd1af840985287ad3221b3fca70f81d745b2c316
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-14 22:19:23 -0700

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -2,4 +2,7 @@ i = int(input())
 newarr = []
 for j in range(i):
     x = (input().split())
-    print(int(x[0])//int(x[1]))
+    try:
+        print(int(x[0])//int(x[1]))
+    except Exception as e:
+        print("Error Code:",e)
