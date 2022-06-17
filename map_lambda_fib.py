e12d01f22187348344928655e18acf36a8fcfc8d
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-16 22:56:38 -0700

    Update map_lambda_fib.py

--- map_lambda_fib.py
+++ map_lambda_fib.py
@@ -2,6 +2,8 @@ cube = lambda x: x**3
 
 def fibonacci(n):
     fibo = [0, 1]
+    if n == 1 or n == 0:
+        return ([1])
     for i in range(n-2):
         fibo.append(fibo[i]+fibo[i+1])
     return fibo
