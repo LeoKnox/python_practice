860ed2c936e8d9bf157867d2aa6c5b12b4a238b0
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-18 18:56:29 -0700

    Update minion_game.py

--- minion_game.py
+++ minion_game.py
@@ -2,13 +2,16 @@ def minion_game(string):
     minions = {}
     vowels = {'A', 'E', 'I', 'O', 'U'}
     i = 0
+    total = 0
     while i < len(string):
-        if string[i] not in vowels:
-            #minions[string[i]] = minions.get(string[i], 0) + 1
-            pass
         i += 1
         for j in range(len(string)):
             minions[string[j:i]] = minions.get(string[j:i], 0) + 1
         for j in range(len(string)):
             print(string[j:i])
     print(minions)
+    minions[''] = 0
+    for m in minions:
+        if m not in vowels or m != '':
+            total += minions[m]
+    print(total)
