f9b96d9ecb7b3fef37cc8f2cb946b0351f62754e
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-21 22:18:53 -0700

    Update ginorts.py

--- ginorts.py
+++ ginorts.py
@@ -6,4 +6,8 @@ for i in oldstring:
         charDict['upper'] = charDict.get('upper', '') + i
     if i.islower():
         charDict['lower'] = charDict.get('lower', '') + i
+    if i.isnumeric() and int(i)%2 == 0:
+        charDict['even'] = charDict.get('even', '') + i
+    if i.isnumeric() and int(i)%2 == 1:
+        charDict['odd'] = charDict.get('odd', '') + i
 print(charDict)
