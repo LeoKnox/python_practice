b6d128ef361179740ed2b0789dcbb359348987fd
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-22 12:08:02 -0700

    Update cloudjumps.py

--- cloudjumps.py
+++ cloudjumps.py
@@ -2,9 +2,9 @@ def jumpingOnClouds(c):
     jumps = 0
     j = []
     for i in range(len(c)-2):
+        j.append([jumps, i])
         if c[i+2] != 1:
             i += 1
         jumps += 1
-        j.append([i, jumps])
     
     return j
