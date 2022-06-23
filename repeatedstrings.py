9c8f89112c0454052286056d326e41af5941e747
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-22 23:19:58 -0700

    Update repeatedstrings.py

--- repeatedstrings.py
+++ repeatedstrings.py
@@ -1,4 +1,8 @@
 def repeatedString(s, n):
     runs = n//len(s)
     cnts = s.count('a')
-    return (cnts)
+    total = runs*cnts
+    for i in range(n%len(s)):
+        if s[i] == 'a':
+            total+=1
+    return (total)
