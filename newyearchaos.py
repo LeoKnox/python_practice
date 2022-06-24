56b6bbb816c1d3131692bbe00fea93f58c07dd6a
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-23 22:24:38 -0700

    Update newyearchaos.py

--- newyearchaos.py
+++ newyearchaos.py
@@ -1,6 +1,8 @@
 def minimumBribes(q):
     bribes = {}
+    total = 0
     for i in range(len(q)):
         if i != q[i]:
+            total += (q[i] - (i+1))
             bribes[i+1] = q[i]
-    print (bribes)
+    print (total)
