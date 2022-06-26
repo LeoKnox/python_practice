8c34cd3ba333e2459024d03787965384f3b88697
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-25 23:03:37 -0700

    Update detectcycle.py

--- detectcycle.py
+++ detectcycle.py
@@ -1,5 +1,10 @@
 def has_cycle(head):
-    if head==None:
-        print(0)
-    else:
-        print(1)
+    slow = head
+    fast = head
+    while fast.next != None and fast != slow:
+        slow = slow.next
+        fast = fast.next.next
+    if fast == slow:
+        return 1
+    if fast.next == None:
+        return 0
