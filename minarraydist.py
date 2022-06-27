ce13aa7f7d4b34c6f52ca268848f3ac4553a11ba
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-26 20:49:46 -0700

    Update minarraydist.py

--- minarraydist.py
+++ minarraydist.py
@@ -2,7 +2,7 @@ def minimumAbsoluteDifference(arr):
     total = 999
     index = 0
     while index < len(arr):
-        temp = index
+        temp = index + 1
         while temp < len(arr):
             if abs(arr[index]-arr[temp]) < total:
                 total = abs(arr[index]-arr[temp])
