2b208295add0f740ccd3f4f890200a6cd6dc40e0
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-27 22:29:18 -0700

    Update luckbalance.py

--- luckbalance.py
+++ luckbalance.py
@@ -2,10 +2,11 @@ def luckBalance(k, contests):
     contests.sort()
     total = 0
     for c in contests[::-1]:
-        if k > 0:
+        if k > 0 and c[1] > 0:
             total += c[0]
-        if k == 0 and c[1] != 0:
+            k -= 1
+        elif k <= 0 and c[1] != 0:
             total -= c[0]
-        if c[1] == 1:
-            k = k - c[1]
+        else:
+            total += c[0]
     return(total)
