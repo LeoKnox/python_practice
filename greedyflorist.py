c6a53a1d2c35263c3a9f7b119c9df9201f8aab64
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-28 22:57:50 -0700

    Update greedyflorist.py

--- greedyflorist.py
+++ greedyflorist.py
@@ -5,7 +5,9 @@ def getMinimumCost(k, c):
     while i >= 0:
         for j in range(n):
             total += ((i//k)+1)*c[i-j]
+            if (i - j) == 0:
+                return total
             alist.append(((i//k)+1)*c[i-j])
             alist.append(i//k)
         i -= k
-    return alist
+    return total
