60946daa4337bfd2c3e6922ad23a0872114fa850
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-01 21:41:38 -0700

    Update fairness.py

--- fairness.py
+++ fairness.py
@@ -1,9 +1,7 @@
 def maxMin(k, arr):
     arr.sort()
     fairness=arr[k]
-    store = [arr]
-    for i in range(len(arr)-k-1):
-        if (arr[i+k-1])-(arr[i]) < fairness:
-            store.append([arr[i+k], arr[i]])
-            fairness = arr[i+k]-arr[i]
-    return(store)
+    for i in range(len(arr)-k):
+        if (arr[(i+k)-1])-(arr[i]) < fairness:
+            fairness = arr[i+k-1]-arr[i]
+    return(fairness)
