86dfa68043f7488e50b4ba917c93533591c57c3c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-01 23:12:10 -0700

    Update divisiblepairs.py

--- divisiblepairs.py
+++ divisiblepairs.py
@@ -1,8 +1,8 @@
 def divisibleSumPairs(n, k, ar):
     pairs = 0
     test = []
-    for i in range(len(ar)):
-        for j in range(i,len(ar)):
+    for i in range(len(ar)-1):
+        for j in range(i+1,len(ar)):
             if (ar[i] + ar[j])%k == 0:
                 test.append([ar[i], ar[j]])
                 pairs += 1
