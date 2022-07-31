b1d571bc8fa26a7c21eddb5a20eb96e167229b92
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-30 22:58:08 -0700

    Update plus_minus.py

--- plus_minus.py
+++ plus_minus.py
@@ -1,16 +1,12 @@
 def plusMinus(arr):
     neg = 0
-    negl = 0
     pos = 0
-    posl = 0
     zero = 0
     for i in arr:
         if i < 0:
             neg += abs(i)
-            negl += 1
         elif i > 0:
             pos += i
-            posl += 1
         else:
             zero += 1
     print (pos/len(arr))
