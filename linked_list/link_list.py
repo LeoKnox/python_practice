b1be17b537be6b201b8680e245ded7fc91d432b9
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-31 22:12:21 -0700

    Update link_list.py

--- linked_list/link_list.py
+++ linked_list/link_list.py
@@ -6,6 +6,11 @@ class Node():
 class List():
   def __init__(self):
     self.head = None
+  def add(self, value):
+    temp = Node(value)
+    temp.next = self.head
+    self.head = temp
 
-a = Node(3)
-print(a.value, a.next)
+a = List()
+a.add(5)
+print(a.head.value, a.head.next)
