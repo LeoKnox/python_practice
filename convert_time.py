0688c3b521d567b20eaacad9445f48981f6b1826
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-01 19:53:46 -0700

    Update convert_time.py

--- convert_time.py
+++ convert_time.py
@@ -1,7 +1,8 @@
 def timeConversion(s):
     s = list(s)
     if s[-2] == 'P':
-        s[0] = int(s[0]) + 1
-        s[1] = int(s[1]) + 2
+        s[0] = str(int(s[0]) + 1)
+        s[1] = str(int(s[1]) + 2)
+    s = list(s)
     "".join(s)
     return(s[0:len(s)-2])
