983dc4c09b30b51461a8ee770f752b7af33ef371
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-04 23:17:19 -0700

    Update tree.py

--- tree/tree.py
+++ tree/tree.py
@@ -7,3 +7,10 @@ class Node():
 class Tree():
   def __init__(self):
     self.head = None
+  def add(self, value):
+    if self.head == None:
+      self.head.value = 3
+
+t = Tree()
+#t.add(3)
+print(t)
