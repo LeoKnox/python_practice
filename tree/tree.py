f107540ed2db833d98904d38298117cacd63a9bd
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-04 23:31:07 -0700

    Update tree.py

--- tree/tree.py
+++ tree/tree.py
@@ -14,11 +14,18 @@ class Tree():
       self.head = temp
     elif value > temp.value:
       if temp.right == None:
-        temp.right = value
+        temp.right = Node(value)
       else:
         print (temp.value)
         temp = temp.right
+    else:
+      if temp.left == None:
+        temp.left = Node(value)
+      else:
+        temp = temp.left
 
 t = Tree()
 t.add(3)
+t.add(4)
+t.add(5)
 print(t.head.value)
