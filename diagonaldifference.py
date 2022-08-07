d8a0764f7c2cb4656e50064acda0d22f1ca30c9e
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-06 23:11:47 -0700

    Update diagonaldifference.py

--- diagonaldifference.py
+++ diagonaldifference.py
@@ -1,9 +1,5 @@
 def diagonalDifference(arr):
     for i in range(0,len(arr)):
-        for j in (len(arr), 0, -1):
+        for j in (len(arr),0):
           print('i', i, 'j', j)
     return("f",len(arr))
-
-
-arr = [[1,2,3], [4,5,6]]
-diagonalDifference(arr)
