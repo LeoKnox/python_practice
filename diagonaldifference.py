59694f717885e6e6e5a6f8843d8d399d68b4cb56
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-06 23:20:10 -0700

    Update diagonaldifference.py

--- diagonaldifference.py
+++ diagonaldifference.py
@@ -1,8 +1,7 @@
 def diagonalDifference(arr):
   first = 0
   second = 0
-  for i in range(0,len(arr)-1):
-    for j in (0,len(arr)-1, -1):
-      first += arr[i][j]
-      second += arr[j][i]
-  return("f",abs(first-second))
+  for i in range(0,len(arr)):
+    first += arr[i][len(arr)-i-1]
+    second += arr[len(arr)-i-1][i]
+  return(abs(first-second))
