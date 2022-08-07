6de6fc5dcc242917332e00cdb5994f1bb16c365c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-06 22:59:08 -0700

    Update lonelyint.py

--- lonelyint.py
+++ lonelyint.py
@@ -3,7 +3,6 @@ def lonelyinteger(a):
     for i in a:
         d[i] = d.get(i, 0)+1
     for key in d.keys():
-      print('a', key)
       if d[key] > 1:
-        print (d[key])
+        return (key)
     return ('x')
