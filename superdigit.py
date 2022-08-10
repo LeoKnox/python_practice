44fac437e1a166ba3636890457b8a3c422e6a914
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-09 22:52:52 -0700

    Update superdigit.py

--- superdigit.py
+++ superdigit.py
@@ -1,9 +1,10 @@
 def superDigit(n, k):
     t = 0
     l = list(n)
-    while (len(n) > 1):
+    while (len(l) > 1):
         for i in l:
             t += int(i)
         l = list(str(t))
-        t = 0    
-    return(t)
+        t = 0
+        k -= 1
+    return(l)
