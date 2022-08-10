68712f98c0014966e7a502adb5d54ab52c90ed3a
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-09 22:31:04 -0700

    Update caesar_cypher.py

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -6,6 +6,8 @@ def caesarCipher(s, k):
         if i.isalpha():
             if (i.islower()) and (ord(i)+k > 122):
                 n += chr(ord(i)+k-26)
+            elif (i.isupper()) and (ord(i) + k > 90):
+                n += chr(ord(i)+k-26)
             else:
                 n += chr(ord(i)+k)
         else:
