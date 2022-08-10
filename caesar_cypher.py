0467a70ac70ffb386843840b83f81ee084caaa60
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-09 22:34:24 -0700

    Update caesar_cypher.py

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,7 +1,6 @@
 def caesarCipher(s, k):
-    #a=97 z=122 A=65 Z=90
-    #return(str(ord('a')) + " " + str(ord('z')))
     n = ""
+    k = k % 26
     for i in s:
         if i.isalpha():
             if (i.islower()) and (ord(i)+k > 122):
