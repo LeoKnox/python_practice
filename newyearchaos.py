ba827ded1286f1cf1e2581325ae4482ebb944517
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-10 23:15:40 -0700

    Update newyearchaos.py

--- newyearchaos.py
+++ newyearchaos.py
@@ -2,8 +2,12 @@ def minimumBribes(q):
     n = []
     for i in range(len(q)):
         if q[i] > i+1:
-            n.append(q[i]-(i+1))
+            diff = q[i]-(i+1)
+            if diff > 2:
+                print('Too chaotic')
+                return()
+            n.append(diff)
             #print(i, q[i], (q[i]-(i+1)))
         #else:
             #print(i, q[i], '0')
-    print(n)
+    print(sum(n))
